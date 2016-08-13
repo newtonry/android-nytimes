a64523d9a4a174aa483f0a4105237acc2aae0469
Ryan Newton <rnewton@hearsaycorp.com>
2016-08-13 09:18:07 -0700

    Use viewholder pattern

--- app/src/main/java/com/fadetoproductions/rvkn/nytimessearch/adapters/ArticleArrayAdapter.java
+++ app/src/main/java/com/fadetoproductions/rvkn/nytimessearch/adapters/ArticleArrayAdapter.java
@@ -21,33 +21,43 @@ import cz.msebera.android.httpclient.util.TextUtils;
  */
 public class ArticleArrayAdapter extends ArrayAdapter<Article> {
 
+    private static class ViewHolder {
+        TextView tvTitle;
+        ImageView imageView;
+    }
+
     public ArticleArrayAdapter(Context context, List<Article> articles) {
-        super(context, android.R.layout.simple_list_item_1);
+        super(context, R.layout.item_article_result);
     }
 
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         Article article = this.getItem(position);
+        ViewHolder viewHolder;
 
         if (convertView == null) {
+            viewHolder = new ViewHolder();
+
             LayoutInflater inflater = LayoutInflater.from(getContext());
             convertView = inflater.inflate(R.layout.item_article_result, parent, false);
+
+            viewHolder.imageView = (ImageView) convertView.findViewById(R.id.ivImage);
+            viewHolder.tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+            convertView.setTag(viewHolder);
+
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
         }
 
-        ImageView imageView = (ImageView) convertView.findViewById(R.id.ivImage);
-        imageView.setImageResource(0);
-
-        TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
-        tvTitle.setText(article.getHeadline());
+        viewHolder.imageView.setImageResource(0);
+        viewHolder.tvTitle.setText(article.getHeadline());
 
         String thumbnail = article.getThumbnailImage();
         if (!TextUtils.isEmpty(thumbnail)) {
-            Picasso.with(getContext()).load(thumbnail).into(imageView);
+            Picasso.with(getContext()).load(thumbnail).into(viewHolder.imageView);
         }
 
         return convertView;
-
-//        return super.getView(position, convertView, parent);
     }
 }
