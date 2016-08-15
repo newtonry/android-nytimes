827bb1202bd83fdf73c7016239b32ff80900ccb2
Ryan Newton <rnewton@hearsaycorp.com>
2016-08-14 20:04:55 -0700

    Refactor a bit

--- app/src/main/java/com/fadetoproductions/rvkn/nytimessearch/fragments/SettingsFragment.java
+++ app/src/main/java/com/fadetoproductions/rvkn/nytimessearch/fragments/SettingsFragment.java
@@ -14,6 +14,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.DatePicker;
@@ -29,6 +30,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import butterknife.BindView;
@@ -47,6 +49,11 @@ public class SettingsFragment extends DialogFragment {
     public ArticleClient articleClient;  // TODO should stop using this pattern
     SearchActivity listener;
     String targetDate;
+    Boolean changesMade;
+    Date beginDate;
+    Date endDate;
+    ArrayList<String> topics;
+
     @BindView(R.id.etBeginDate) EditText etEndDate;
     @BindView(R.id.etEndDate) EditText etBeginDate;
     @BindView(R.id.btnSave) Button btnSave;
@@ -74,6 +81,7 @@ public class SettingsFragment extends DialogFragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         ButterKnife.bind(this, view);
+        changesMade = false;
         setupListeners();
         populateFields();
 
@@ -82,6 +90,11 @@ public class SettingsFragment extends DialogFragment {
     public void populateFields() {
         // Is there a better way to render this than with all these if statements
         List<String> sortArray = Arrays.asList(getResources().getStringArray(R.array.sort_options));
+
+        topics = articleClient.topics;
+        beginDate = articleClient.beginDate;
+        endDate = articleClient.endDate;
+
         if (articleClient.getSort() != null) {
             int indexOfOption = sortArray.indexOf(articleClient.getSort());
             spnrSort.setSelection(indexOfOption);
@@ -137,6 +150,19 @@ public class SettingsFragment extends DialogFragment {
             @Override
             public void onClick(View v) { onCheckboxClicked();}
         });
+
+        spnrSort.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                changesMade = true;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
     }
 
     public void openDatePickerDialogue() {
@@ -151,6 +177,7 @@ public class SettingsFragment extends DialogFragment {
     }
 
     public void datePicked(int year, int month, int day) {
+        changesMade = true;
         Calendar calendar = Calendar.getInstance();
         calendar.set(year, month, day);
 
@@ -159,18 +186,19 @@ public class SettingsFragment extends DialogFragment {
 
         if (targetDate.equals("beginDate")) { // This seems really dumb
             // TODO don't set the article client here. wait for save
-            articleClient.beginDate = calendar.getTime();
+            beginDate = calendar.getTime();
             etBeginDate.setText(dateString);
         }
         if (targetDate.equals("endDate")) { // This seems really dumb
             // TODO don't set the article client here. wait for save
-            articleClient.endDate = calendar.getTime();
+            endDate = calendar.getTime();
             etEndDate.setText(dateString);
         }
     }
 
     public void onCheckboxClicked() {
-        ArrayList<String> topics = new ArrayList<>();
+        changesMade = true;
+        topics.clear();
         if (cbArts.isChecked()) {
             topics.add(getResources().getString(R.string.topic_arts));
         }
@@ -180,8 +208,6 @@ public class SettingsFragment extends DialogFragment {
         if (cbSports.isChecked()) {
             topics.add(getResources().getString(R.string.topic_sports));
         }
-        // TODO don't set the article client here. wait for save
-        articleClient.topics = topics;
     }
 
     @Override
@@ -200,11 +226,14 @@ public class SettingsFragment extends DialogFragment {
     }
 
     public void onSave() {
-        Boolean changesMade = true;
-        // Get the values
-        String sort = spnrSort.getSelectedItem().toString();
-        articleClient.setSort(sort);
-        listener.onFinishDialog(changesMade);
+        if (changesMade) {
+            String sort = spnrSort.getSelectedItem().toString();
+            articleClient.setSort(sort);
+            articleClient.topics = topics;
+            articleClient.beginDate = beginDate;
+            articleClient.endDate = endDate;
+            listener.onFinishDialog(changesMade);
+        }
         dismiss();
     }
 }
